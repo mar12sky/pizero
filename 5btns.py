679e84dd369a3ba7fc6abe3aa8017c5841096c82
mar12sky <mar12sky@users.noreply.github.com>
2025-05-28 21:44:59 +0530

    Create 5btns.py

---
+++ 5btns.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import os
+
+# Define GPIO pins for buttons
+BUTTON_PINS = [2, 3, 4, 17, 27]  # Adjust these pins as needed
+WALLPAPERS = [
+    "/home/pi/Pictures/wallpaper1.jpg",
+    "/home/pi/Pictures/wallpaper2.jpg",
+    "/home/pi/Pictures/wallpaper3.jpg",
+    "/home/pi/Pictures/wallpaper4.jpg",
+    "/home/pi/Pictures/wallpaper5.jpg"
+]
+
+# Setup GPIO
+GPIO.setmode(GPIO.BCM)
+for pin in BUTTON_PINS:
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def change_wallpaper(channel):
+    index = BUTTON_PINS.index(channel)
+    wallpaper = WALLPAPERS[index]
+    os.system(f"pcmanfm --set-wallpaper {wallpaper}")
+    print(f"Wallpaper changed to: {wallpaper}")
+
+# Register event detection
+for pin in BUTTON_PINS:
+    GPIO.add_event_detect(pin, GPIO.FALLING, callback=change_wallpaper, bouncetime=300)
+
+try:
+    print("Waiting for button presses to change wallpaper...")
+    while True:
+        pass  # Keep the script running
+
+except KeyboardInterrupt:
+    print("Exiting...")
+    GPIO.cleanup()
